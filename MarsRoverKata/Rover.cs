e249f142232edef4ccb263864d76e2c16d506812
Mehdi-Lucas <71726210+Mehdi-Lucas@users.noreply.github.com>
2023-01-24 09:45:23 +0100

    improved Rover class obstacle detection, report method and added comments

--- MarsRoverKata/Rover.cs
+++ MarsRoverKata/Rover.cs
@@ -8,16 +8,31 @@ namespace MarsRoverKata
 {
     public class Rover
     {
+        /// <summary>
+        /// Gets the rover position in the grid.
+        /// </summary>
         public GridPoint position { get; private set; }
+
+        /// <summary>
+        /// Gets the direction the rover is facing.
+        /// </summary>
         public Direction Heading { get; private set; }
 
+        /// <summary>
+        /// Intialize a new rover instance.
+        /// </summary>
+        /// <param name="startPt">The point on the grid where the rover will start.</param>
+        /// <param name="startDirection">The direction the rover will be facing.</param>
         public Rover(GridPoint startPt, Direction startDirection)
         {
             this.position = startPt;
             this.Heading = startDirection;
-            Console.WriteLine(this);
+            SendSuccessReport("was dropped on Mars");
         }
 
+        /// <summary>
+        /// Turn the rover to face the direction on the left of the current one.
+        /// </summary>
         public void TurnLeft()
         {
             switch (this.Heading)
@@ -38,9 +53,12 @@ namespace MarsRoverKata
                     throw new Exception("undefined heading!");
             }
 
-            Console.WriteLine($"Report: I turned left and now I'm facing the {Heading}!");
+            SendSuccessReport("turned left");
         }
 
+        /// <summary>
+        /// Turn the rover to face the direction on the right of the current one.
+        /// </summary>
         public void TurnRight()
         {
             switch (this.Heading)
@@ -61,9 +79,12 @@ namespace MarsRoverKata
                     throw new Exception("undefined heading!");
             }
 
-            Console.WriteLine($"Report: I turned right and now I'm facing the {Heading}!");
+            SendSuccessReport("turned right");
         }
 
+        /// <summary>
+        /// Move the rover forward toward the direction it is facing.
+        /// </summary>
         public void MoveForward()
         {
             GridPoint nextPoint = position.GetNextForwardPoint(this.Heading);
@@ -76,27 +97,48 @@ namespace MarsRoverKata
                 this.position = nextPoint;
             }
 
-            Console.WriteLine(this);
+            SendSuccessReport("moved forward");
         }
 
+        /// <summary>
+        /// Move the rover backward away from the direction it is facing.
+        /// </summary>
         public void MoveBackward()
         {
             GridPoint nextPoint = position.GetNextBackwardPoint(this.Heading);
-            if (nextPoint.Equals(RoverProgram.obstacle))
+            if (DetectObstacle(nextPoint))
             {
                 throw new Exception($"Report: Obstacle encountered at {nextPoint}, aborting sequence!");
             }
-            else
+
+            this.position = nextPoint;
+
+            SendSuccessReport("moved backward");
+        }
+
+        /// <summary>
+        /// Check if there is an obstacle in the next grid point.
+        /// </summary>
+        /// <param name="gridPoint">The GridPoint to evaluate.</param>
+        /// <returns>A bool indicating an obstacle presence.</returns>
+        public bool DetectObstacle(GridPoint gridPoint)
+        {
+            if (gridPoint.Equals(RoverProgram.obstacle))
             {
-                this.position = nextPoint;
+                return true;
             }
 
-            Console.WriteLine(this);
+            return false;
+        }
+
+        public void SendSuccessReport(string action)
+        {
+            Console.WriteLine($"Report: I {action}, {this}");
         }
 
         public override string ToString()
         {
-            return $"Report: i'm at {position} and I'm facing the {Heading}!";
+            return $"i'm at {position} and I'm facing the {Heading}!";
         }
     }
 }
